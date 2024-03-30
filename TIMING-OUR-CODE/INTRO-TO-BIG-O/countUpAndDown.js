ad8284b6b544d6ef10cfcee7df3a4911b710c8a4
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-03-30 23:56:23 +0400

    Add countUpAndDown.js

---
+++ TIMING-OUR-CODE/INTRO-TO-BIG-O/countUpAndDown.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function countUpAndDown(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+
+  for (let j = n - 1; j > 0; j--) {
+    console.log(j);
+  }
+}
+
+countUpAndDown(20);
