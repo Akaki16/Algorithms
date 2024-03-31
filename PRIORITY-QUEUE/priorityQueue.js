69122347c61ad9af3ba9eb8188c6cac92628edbb
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2024-04-01 02:01:37 +0400

    Add priorityQueue.js

---
+++ PRIORITY-QUEUE/priorityQueue.js
@@ -0,0 +1,88 @@
+'use strict';
+
+class Node {
+  constructor(val, priority) {
+    this.value = val;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val, priority) {
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      const parent = this.values[parentIndex];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
+
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swapIndex = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.priority < element.priority) {
+          swapIndex = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swapIndex === null && rightChild.priority < element.priority) ||
+          (swapIndex !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swapIndex = rightChildIndex;
+        }
+      }
+
+      if (swapIndex === null) break;
+      this.values[index] = this.values[swapIndex];
+      this.values[swapIndex] = element;
+      index = swapIndex;
+    }
+  }
+}
+
+const priorityQueue = new PriorityQueue();
+
+priorityQueue.enqueue('eat', 1);
+priorityQueue.enqueue('study algorithms and data structures', 2);
+priorityQueue.enqueue('study math', 3);
+priorityQueue.enqueue('play video games', 10);
+
+console.log(priorityQueue);
+console.log(priorityQueue.dequeue());
+console.log(priorityQueue);
